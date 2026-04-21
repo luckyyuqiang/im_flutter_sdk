09de2e088441e2bae79d2663132dc35d116954bd
luckyyuqiang <luckyyuqiang@163.com>
2026-04-21 16:17:14 +0800

    add testing code for sending paral big messages.

--- im_flutter_sdk/example/lib/main.dart
+++ im_flutter_sdk/example/lib/main.dart
@@ -3,7 +3,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:im_flutter_sdk/im_flutter_sdk.dart';
 
-var appKey = "easemob#easeim";
+//var appKey = "easemob-demo#wang";
+var appKey = "easemob-demo#ngisdkdemo";
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,6 +13,12 @@ void main() async {
   EMOptions options = EMOptions.withAppKey(
     appKey,
     autoLogin: false,
+    debugMode: true,
+    //enableDNSConfig: false,
+    //restServer: "https://a1-qa-hsb.easemob.com",
+    //webSocketServer: "im-api-ws1-qa-hsb.easemob.com",
+    //webSocketPort: 443,
+    //enableTLS: true,
   );
 
   await EMClient.getInstance.init(options);
@@ -138,6 +145,30 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               child: const Text("SEND TEXT"),
             ),
+            TextButton(
+              onPressed: _sendCustomBigMessage,
+              style: ButtonStyle(
+                foregroundColor: WidgetStateProperty.all(Colors.white),
+                backgroundColor: WidgetStateProperty.all(Colors.lightBlue),
+              ),
+              child: const Text("SEND CUSTOM BIG MESSAGE"),
+            ),
+            TextButton(
+              onPressed: _sendCustomLittleMessage,
+              style: ButtonStyle(
+                foregroundColor: WidgetStateProperty.all(Colors.white),
+                backgroundColor: WidgetStateProperty.all(Colors.lightBlue),
+              ),
+              child: const Text("SEND CUSTOM LITTLE MESSAGE"),
+            ),
+            TextButton(
+              onPressed: _joinChatRoom,
+              style: ButtonStyle(
+                foregroundColor: WidgetStateProperty.all(Colors.white),
+                backgroundColor: WidgetStateProperty.all(Colors.lightBlue),
+              ),
+              child: const Text("JOIN CHAT ROOM"),
+            ),
             Flexible(
               child: ListView.builder(
                 controller: scrollController,
@@ -193,7 +224,7 @@ class _MyHomePageState extends State<MyHomePage> {
       "UNIQUE_HANDLER_ID",
       ChatMessageEvent(
         onSuccess: (msgId, msg) {
-          _addLogToConsole("on message succeed");
+          //_addLogToConsole("on message succeed");
         },
         onProgress: (msgId, progress) {
           _addLogToConsole("on message progress");
@@ -310,6 +341,66 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  EMMessage _createCustomBigMessage(String userId, String inEvent) {
+      const String _messageContent = '''
+      {
+        "{"type":"GIFT","badgeUrls":["https://miggo.oss-ap-southeast-1.aliyuncs.com/other/manager-15f1274c-672c-4981-92ad-ea7da88d1086.png"],"vipInfo":{"type":"MARQUIS","badgeUrl":"https://miggo.oss-ap-southeast-1.aliyuncs.com/svga_cover/manager-a48640ae-9dcf-45a3-88b0-12b850ccd8c7.png"},"userProfile":{"id":"2001635932659589121","account":"10048","userAvatar":"https://dev-yuyin.oss-ap-southeast-1.aliyuncs.com/avatar/c311e981-2080-4ff9-bdf5-110329924ba1.jpg","userNickname":"box1","userSex":1,"age":29,"freezingTime":1766004570000,"countryId":"1231833304232112130","countryName":"India","countryCode":"IN","regionCode":"OTHER","originSys":"MIGGO","sysOriginChild":"MIGGO","del":false,"createTime":1766090970000,"bornYear":1996,"bornMonth":1,"bornDay":1,"useProps":[{"userId":"2001635932659589121","propsResources":{"id":"2005473358830690306","type":"NOBLE_VIP","code":"VIP3","name":"MARQUIS","cover":"https://miggo.oss-ap-southeast-1.aliyuncs.com/svga_cover/manager-a48640ae-9dcf-45a3-88b0-12b850ccd8c7.png","sourceUrl":"https://miggo.oss-ap-southeast-1.aliyuncs.com/svgasource/manager-c1e11c97-074d-4e28-8b76-4914d4aa9efa.svga","expand":"","amount":30000.0},"expireTime":1769582552000,"allowGive":null}],"wearBadge":[{"id":"1565533277562978305","badgeLevel":5,"milestone":"","badgeName":"SVIP5","type":"ACTIVITY","badgeKey":"svip5_badge","selectUrl":"https://miggo.oss-ap-southeast-1.aliyuncs.com/other/manager-15f1274c-672c-4981-92ad-ea7da88d1086.png","notSelectUrl":"","animationUrl":"https://miggo.oss-ap-southeast-1.aliyuncs.com/other/manager-7f5f13b8-0830-4c19-b4f9-3ffcf05eb69b.svga"}],"ownSpecialId":null,"userLevel":null,"inRoomId":null,"roomIcon":null,"accountStatus":"NORMAL","sameRegion":true,"isUpdateCountry":"1"},"chatBubble":"","giftMsg":{"sender":{"id":"2001635932659589121","userName":"box1","userPic":"https://dev-yuyin.oss-ap-southeast-1.aliyuncs.com/avatar/c311e981-2080-4ff9-bdf5-110329924ba1.jpg"},"sendGiftAllType":"ON_MIC","giftList":[{"giftId":"2006262610737274881","giftPic":"https://miggo.oss-ap-southeast-1.aliyuncs.com/gifts/manager-83303ab8-9d49-498b-80b7-9a73c91d7994.png","giftSourceUrl":"","special":"","currencyType":"","giftTab":"LUCKY_GIFT","giftCount":1,"giftPrice":10,"rewardMultiple":1}],"receiverList":[{"receiverId":"2001635932659589121","receiverName":"box1","receiverAvatar":"https://dev-yuyin.oss-ap-southeast-1.aliyuncs.com/avatar/c311e981-2080-4ff9-bdf5-110329924ba1.jpg","jumpCombo":8,"giftIds":["2006262610737274881"]}],"sendGiftTime":1767773894989,"target":1},"svipLevel":"SVIP_5"}"
+      }
+      ''';
+
+    EMMessage msg = EMMessage.createCustomSendMessage(
+      targetId: userId,
+      event: inEvent,
+      params: {
+        "data": _messageContent,
+      },
+    );
+    msg.chatType = ChatType.ChatRoom;
+    return msg;
+  }
+
+    EMMessage _createCustomLittleMessage(String userId, String inEvent) {
+      const String _messageContent = '''
+      {
+        "{"type":"GIFT","badgeUrls":["https://miggo.oss-ap-southeast-1.aliyuncs.com/other/ea7da88d1086.png"]}"
+      }
+      ''';
+
+    EMMessage msg = EMMessage.createCustomSendMessage(
+      targetId: userId,
+      event: inEvent,
+      params: {
+        "data": _messageContent,
+      },
+    );
+    msg.chatType = ChatType.ChatRoom;
+    return msg;
+  }
+
+  void _sendCustomBigMessage() async {
+    if (_chatId.isEmpty) {
+      _addLogToConsole("chat id is null");
+      return;
+    }
+    for (var i = 0; i < 10; i++) {
+      var msg = _createCustomBigMessage(_chatId, "ROOM_CHAT_$i");
+      await EMClient.getInstance.chatManager.sendMessage(msg);
+      _addLogToConsole("send big message done");
+    }
+  }
+
+  void _sendCustomLittleMessage() async {
+    if (_chatId.isEmpty) {
+      _addLogToConsole("chat id is null");
+      return;
+    }
+    for (var i = 0; i < 10; i++) {
+      var msg = _createCustomLittleMessage(_chatId, "ROOM_CHAT_$i");
+      await EMClient.getInstance.chatManager.sendMessage(msg);
+      _addLogToConsole("send little message done");
+    }
+  }
+
   void _sendMessage() async {
     if (_chatId.isEmpty || _messageContent.isEmpty) {
       _addLogToConsole("single chat id or message content is null");
@@ -322,6 +413,20 @@ class _MyHomePageState extends State<MyHomePage> {
     );
 
     await EMClient.getInstance.chatManager.sendMessage(msg);
+  }
+
+  void _joinChatRoom() async {
+    if (_chatId.isEmpty) {
+      _addLogToConsole("chat id is null");
+      return;
+    }
+    try {
+      _addLogToConsole("join chat room...");
+      await EMClient.getInstance.chatRoomManager.joinChatRoom(_chatId);
+      _addLogToConsole("join chat room succeed");
+    } on EMError catch (e) {
+      _addLogToConsole("join chat room failed, e: ${e.code} , ${e.description}");
+    }
   }
 
   void _addLogToConsole(String log) {
